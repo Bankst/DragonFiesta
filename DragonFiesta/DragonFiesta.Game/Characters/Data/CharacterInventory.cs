c0b7e50fc36c2c192d8e46f025ada6fc02e8709e
Sequess <noeller95@web.de>
2018-07-18 17:30:06 +0200

    Merge Feature-SHNLoad

--- DragonFiesta/DragonFiesta.Game/Characters/Data/CharacterInventory.cs
+++ DragonFiesta/DragonFiesta.Game/Characters/Data/CharacterInventory.cs
@@ -1,50 +1,49 @@
 ﻿using System.Collections.Concurrent;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using DragonFiesta.Providers.Items;
 
 namespace DragonFiesta.Game.Characters.Data
 {
-	public class CharacterInventory
-	{
-		public ConcurrentDictionary<uint, ItemBaseInfo> CharacterInventoryData { get; set; }
-		
-		public bool RefreshFromSQL(SQLResult pResult, int i)
-		{
-			CharacterInventoryData = new ConcurrentDictionary<uint, ItemBaseInfo>();
+    public class CharacterInventory
+    {
+        public ConcurrentDictionary<uint, ItemBaseInfo> CharacterInventoryData { get; set; }
 
-			var charID = pResult.Read<long>(i, "ID");
+        public bool RefreshFromSQL(SQLResult pResult, int i)
+        {
+            CharacterInventoryData = new ConcurrentDictionary<uint, ItemBaseInfo>();
 
-			using (var iResult = DB.Select(DatabaseType.World, "SELECT * FROM Items WHERE Owner = @pOwner",
-				new SqlParameter("@pOwner", charID)))
-			{
-				foreach (DataRow item in iResult.Rows)
-				{
-					var ItemKey = (long) item["ItemKey"];
-//					var StorageType = (byte) item["StorageType"];
-//					var Owner = (long) item["Owner"];
-//					vart Storage = (short) item["Storage"];
-//					var ItemID = (int) item["ItemID"];
-//					var Flags = (byte) item["Flags"];
+            var charID = pResult.Read<long>(i, "ID");
 
-					using (var ioResult = DB.Select(DatabaseType.World, "SELECT * FROM ItemOptions WHERE ItemKey = @pItemKey",
-						new SqlParameter("@pItemKey", ItemKey)))
-					{
-						 // TODO: figure out ItemOption and OptionTypes
-					}
+            using (var iResult = DB.Select(DatabaseType.World, "SELECT * FROM Items WHERE Owner = @pOwner",
+                new SqlParameter("@pOwner", charID)))
+            {
+                foreach (DataRow item in iResult.Rows)
+                {
+                    var ItemKey = (long)item["ItemKey"];
+                    //					var StorageType = (byte) item["StorageType"];
+                    //					var Owner = (long) item["Owner"];
+                    //					vart Storage = (short) item["Storage"];
+                    //					var ItemID = (int) item["ItemID"];
+                    //					var Flags = (byte) item["Flags"];
 
-				}
-			}
-			
-			return true;
-		}
+                    using (var ioResult = DB.Select(DatabaseType.World, "SELECT * FROM ItemOptions WHERE ItemKey = @pItemKey",
+                        new SqlParameter("@pItemKey", ItemKey)))
+                    {
+                        // TODO: figure out ItemOption and OptionTypes
+                    }
 
-		
+                }
+            }
 
-		~CharacterInventory()
-		{
-			CharacterInventoryData = null;
-		}
-	}
+            return true;
+        }
+
+
+
+        ~CharacterInventory()
+        {
+            CharacterInventoryData = null;
+        }
+    }
 }
